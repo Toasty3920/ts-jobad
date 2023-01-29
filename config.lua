fef02829a6adfecba52030253d4cd6ce484aac63
TS-Shop <119107482+Toasty3920@users.noreply.github.com>
2023-01-29 21:01:06 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.CommandName = "jobad"            -- Set the commandname here
+
+Config.PoliceJobName = "police"
+Config.AmbulanceJobName = "ambulance"
+Config.MechanicJobName = "mechanic"
