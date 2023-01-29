8f00b75d1729dc72e24d0715343eabd870a5f660
TS-Shop <119107482+Toasty3920@users.noreply.github.com>
2023-01-29 21:00:33 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Toasty - TS-Scripts'
+description 'tsJobAd from Toasty - TS-Shop. Send jobads easily to all players with this scripts'
+
+client_scripts {
+    'config.lua',
+    'client/client.lua',
+}
+
+server_scripts {
+    'config.lua',
+    'server/server.lua',
+}
