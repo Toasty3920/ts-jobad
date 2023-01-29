2cd5f2afc0ace4da1684c7c67d83673ca7e571fb
TS-Shop <119107482+Toasty3920@users.noreply.github.com>
2023-01-29 21:01:30 +0100

    Create client.lua

---
+++ client/client.lua
@@ -0,0 +1,27 @@
+ESX = exports['es_extended']:getSharedObject()
+
+RegisterCommand(Config.CommandName, function(source, args, raw)
+    ESX.TriggerServerCallback("tsJobAd:getPlayerJob", function(jobname)
+        if jobname == Config.PoliceJobName then
+            TriggerServerEvent("tsJobAd:sendPoliceJobAd", args)
+        elseif jobname == Config.AmbulanceJobName then
+            TriggerServerEvent("tsJobAd:sendAmbulanceJobAd", args)
+        elseif jobname == Config.MechanicJobName then
+            TriggerServerEvent("tsJobAd:sendMechanicJobAd", args)
+        else
+            ESX.ShowNotification("Du kannst keine Mitteilungen versenden!")
+        end
+    end)
+end)
+
+RegisterNetEvent('tsJobAd:sendNotification')
+AddEventHandler('tsJobAd:sendNotification', function(title, subtitle, msg, icon)
+    showPictureNotification(title, subtitle, msg, icon)
+end)
+
+function showPictureNotification(title, subtitle, msg, icon)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(msg)
+    SetNotificationMessage(icon, icon, true, 1, title, subtitle)
+    DrawNotification(false, true)
+end
